39654de6d6e9c8704c29dc8905bf4dcb0ac77663
Igor Lima <limasigor2@gmail.com>
2018-11-15 10:22:38 -0300

    first commit

---
+++ simplemooc/simplemooc/core/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from simplemooc.core.views import home
+from simplemooc.core.views import contact
+
+app_name = 'core'
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('contato/', contact, name='contact'),
+    path('admin/', admin.site.urls),
+]
